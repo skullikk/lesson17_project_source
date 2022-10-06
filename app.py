e83fe8106afbfc362227a5953eb9aab4a904ba07
skullikk <skullikk@mail.ru>
2022-10-06 10:43:04 +0300

    Create project

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ from marshmallow import Schema, fields
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_SORT_KEYS'] = False
+app.config['RESTX_JSON'] = {'ensure_ascii': False, 'indent': 2}
 db = SQLAlchemy(app)
+
+api = Api(app)
+movies_ns = api.namespace('movies')
 
 
 class Movie(db.Model):
@@ -24,6 +29,19 @@ class Movie(db.Model):
     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
     director = db.relationship("Director")
 
+
+# Создали схему Movie
+class MovieSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+    genre_id = fields.Int()
+    director_id = fields.Int()
+
+
 class Director(db.Model):
     __tablename__ = 'director'
     id = db.Column(db.Integer, primary_key=True)
@@ -35,6 +53,44 @@ class Genre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
 
+
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+
+
+# Создаем представлениe для всех фильмов
+@movies_ns.route('/')
+class MoviesView(Resource):
+    def get(self):
+        # Проверяем наличие ключей в url
+        director_id = request.args.get('director_id')
+        genre_id = request.args.get('genre_id')
+        # Если есть ключ director_id - получаем его и выводим список фильмов. Если список пустой - выводим "Empty"
+        if director_id:
+            movies = Movie.query.filter(Movie.director_id == director_id).all()
+            if movies:
+                return movies_schema.dump(movies), 200
+            else:
+                return 'Empty', 200
+        # Если есть ключ genre_id - получаем его и выводим список фильмов. Если список пустой - выводим "Empty"
+        elif genre_id:
+            movies = Movie.query.filter(Movie.genre_id == genre_id).all()
+            if movies:
+                return movies_schema.dump(movies), 200
+            else:
+                return 'Empty', 200
+
+        else:
+            all_movies = Movie.query.all()
+            return movies_schema.dump(all_movies), 200
+
+
+# Создаем представление для фильма по его id
+@movies_ns.route('/<int:mid>')
+class MovieView(Resource):
+    def get(self, mid):
+        movie = Movie.query.filter(Movie.id == mid).one()
+        return movie_schema.dump(movie), 200
 
 
 if __name__ == '__main__':
